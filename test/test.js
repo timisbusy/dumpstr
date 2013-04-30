f9d81190fbed55dd558017274758a61b38f8b69f
Tim Atkinson <timisbusy@gmail.com>
2013-04-30 17:15:12 -0400

    adding loglevel and optional logging

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 var dumpstr = require('..');
 var datestr = +new Date();
+dumpstr.setLogLevel(1);
 dumpstr.dump("mongodb://localhost/test", "test-" + datestr, function (err, res) {
   console.log(arguments);
 });
