bd8c6a04a06e9803aeb3e8d6b337f00659e70935
Tim Atkinson <timisbusy@gmail.com>
2013-05-03 14:41:55 -0400

    adding options

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var dumpstr = require('..');
 var datestr = +new Date();
 dumpstr.setLogLevel(1);
-dumpstr.dump("mongodb://localhost/test", "test-" + datestr, function (err, res) {
+dumpstr.dump("mongodb://localhost/test", "test-" + datestr, { partSize: 2*10485760 }, function (err, res) {
   console.log(arguments);
 });
